eac5319c3fc31505fc221f8dd052b6ff4541e59c
Eben Blaisdell <emb038@bucknell.edu>
2017-02-11 02:23:13 -0500

    loot updates

--- public/loot.js
+++ public/loot.js
@@ -2,7 +2,7 @@ imageNames = ["trophy","patch-outside","patch-inside"];
 images = {};
 
 // TODO: Get loot info from DB
-loot = [{type:"trophy",text:"test",primColor:"#0000FF",secColor:"#0000BB"},{type:"patch",text:"winning"}];
+loot = [{type:"trophy",text:"test",primColor:"0,0,255",secColor:"0,0,127"},{type:"patch",text:"winning",primColor:"0,0,255",secColor:"0,0,127"}];
 
 $(window).on("load", function(){
 	imageNames.forEach(function(name,i){
@@ -20,15 +20,33 @@ $(window).on("load", function(){
 				ctx.imageSmoothingEnabled = false;
 				ctx.drawImage(images.trophy, 0, 0, c.width, c.height);
 				ctx.imageSmoothingEnabled = true;
-				ctx.fillStyle = item.secColor;
+				ctx.fillStyle = cssColor(item.secColor);
 				ctx.font = "bold 64px sans-serif";
 				ctx.textAlign = "center"
 				ctx.fillText(item.text, 0.5*c.width, 0.76*c.height);
-				ctx.fillStyle = item.primColor;
+				ctx.fillStyle = cssColor(item.primColor);
 				ctx.fillText(item.text, 0.5*c.width, 0.75*c.height)
 				break;
 			case "patch":
-				
+				ctx.imageSmoothingEnabled = false;
+				ctx.drawImage(images["patch-outside"], 0, 0.325*c.height, c.width, 0.25*c.height);
+				ctx.fillStyle = cssColor(item.primColor,0.5);
+				ctx.fillRect(0, 0.325*c.height, c.width, 0.25*c.height);
+				ctx.drawImage(images["patch-inside"], 0, 0.325*c.height, c.width, 0.25*c.height);
+				ctx.fillStyle = cssColor(item.primColor,0.5);
+				px = c.width/64;
+				ctx.fillRect(3*px, 3*px+0.325*c.height, c.width - 6*px, 0.25*c.height - 6*px);
+				ctx.imageSmoothingEnabled = true;
+				ctx.fillStyle = cssColor(item.primColor);
+				ctx.font = "bold 48px sans-serif";
+				ctx.textAlign = "center"
+				ctx.fillText(item.text, 0.5*c.width, 0.5*c.height);
+				break;
 		}
 	})
 })
+
+function cssColor(param,alpha){
+	alpha = alpha || "1";
+	return "rgba("+param+","+alpha+")";
+}
