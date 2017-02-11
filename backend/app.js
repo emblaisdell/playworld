184dc8f614af7478c1bdd418d7320208ada72eca
Eben Blaisdell <emb038@bucknell.edu>
2017-02-11 01:39:54 -0500

    app static file fixes

--- backend/app.js
+++ backend/app.js
@@ -4,12 +4,15 @@ var express = require('express')
 var mongoose = require('mongoose')
 
 var app = express()
+path = require('path');
 
 const User = mongoose.model('User')
 
 app.get('/', function (req, response) {
   response.send('Hello, world!')
 })
+
+app.use('/webapp', express.static(path.join(__dirname + '/../public')));
 
 app.listen(3000, function () {
   console.log('App listening on port', 3000)
