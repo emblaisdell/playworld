ff4f929fdcc61090e8926925c48d566846e8e710
Eben Blaisdell <emb038@bucknell.edu>
2017-02-11 09:39:01 -0500

    front end upgrades

--- public/loot.js
+++ public/loot.js
@@ -2,7 +2,11 @@ imageNames = ["trophy","patch-outside","patch-inside"];
 images = {};
 
 // TODO: Get loot info from DB
-loot = [{type:"trophy",text:"test",primColor:"0,0,255",secColor:"0,0,127"},{type:"patch",text:"winning",primColor:"0,0,255",secColor:"0,0,127"}];
+loot = [
+	{type:"trophy",text:"test",primColor:"0,0,255",secColor:"0,0,127"},
+	{type:"patch",text:"Winning",primColor:"0,0,255",secColor:"0,127,0"},
+	{type:"trophy",text:"You're Mom",primColor:"255,0,0",secColor:"255,127,0"}
+];
 
 $(window).on("load", function(){
 	imageNames.forEach(function(name,i){
@@ -21,7 +25,7 @@ $(window).on("load", function(){
 				ctx.drawImage(images.trophy, 0, 0, c.width, c.height);
 				ctx.imageSmoothingEnabled = true;
 				ctx.fillStyle = cssColor(item.secColor);
-				ctx.font = "bold 64px sans-serif";
+				ctx.font = "bold 48px sans-serif";
 				ctx.textAlign = "center"
 				ctx.fillText(item.text, 0.5*c.width, 0.76*c.height);
 				ctx.fillStyle = cssColor(item.primColor);
@@ -33,12 +37,12 @@ $(window).on("load", function(){
 				ctx.fillStyle = cssColor(item.primColor,0.5);
 				ctx.fillRect(0, 0.325*c.height, c.width, 0.25*c.height);
 				ctx.drawImage(images["patch-inside"], 0, 0.325*c.height, c.width, 0.25*c.height);
-				ctx.fillStyle = cssColor(item.primColor,0.5);
+				ctx.fillStyle = cssColor(item.secColor,0.5);
 				px = c.width/64;
 				ctx.fillRect(3*px, 3*px+0.325*c.height, c.width - 6*px, 0.25*c.height - 6*px);
 				ctx.imageSmoothingEnabled = true;
 				ctx.fillStyle = cssColor(item.primColor);
-				ctx.font = "bold 48px sans-serif";
+				ctx.font = "bold 32px sans-serif";
 				ctx.textAlign = "center"
 				ctx.fillText(item.text, 0.5*c.width, 0.5*c.height);
 				break;
---
+++ public/scripts.js
@@ -0,0 +1,42 @@
+function setCookie(c_name,value,exdays)
+{
+	var exdate=new Date();
+	exdate.setDate(exdate.getDate() + exdays);
+	var c_value=escape(value) + 
+	((exdays==null) ? "" : ("; expires="+exdate.toUTCString()));
+	document.cookie=c_name + "=" + c_value;
+}
+
+function getCookie(c_name)
+{
+	var i,x,y,ARRcookies=document.cookie.split(";");
+	for (i=0;i<ARRcookies.length;i++){
+		x=ARRcookies[i].substr(0,ARRcookies[i].indexOf("="));
+		y=ARRcookies[i].substr(ARRcookies[i].indexOf("=")+1);
+		x=x.replace(/^\s+|\s+$/g,"");
+		if (x==c_name)
+		{
+			return unescape(y);
+		}
+	}
+}
+
+function parseURLParams(url) {
+	var queryStart = url.indexOf("?") + 1,
+		queryEnd   = url.indexOf("#") + 1 || url.length + 1,
+		query = url.slice(queryStart, queryEnd - 1),
+		pairs = query.replace(/\+/g, " ").split("&"),
+		parms = {}, i, n, v, nv;
+
+	if (query === url || query === "") return;
+
+	for (i = 0; i < pairs.length; i++) {
+		nv = pairs[i].split("=", 2);
+		n = decodeURIComponent(nv[0]);
+		v = decodeURIComponent(nv[1]);
+
+		if (!parms.hasOwnProperty(n)) parms[n] = [];
+		parms[n].push(nv.length === 2 ? v : null);
+	}
+	return parms;
+}
